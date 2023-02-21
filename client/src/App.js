38e58ddb17511257e8171bae9686a7c5f0f8f392
Victor <v.henriquedasilva10@gmail.com>
2023-02-21 15:59:48 -0300

    building frontend

---
+++ client/src/App.js
@@ -0,0 +1,85 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          <span className="color-text">Shark</span>Tech
+        </a>
+
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>Tecnologia e Trabalho Home Office</h2>
+          <p className="info">
+            <a className="author">Victor Henrique</a>
+            <time>2023-01-06 16:41</time>
+          </p>
+          <p className="summary">
+            The href attribute requires a valid value to be accessible. Provide
+            a valid, navigable address as the href value. If you cannot provide
+            a valid href, but still need the element to resemble a link, use a
+            button and change it with appropriate styles. Learn more:
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>Tecnologia e Trabalho Home Office</h2>
+          <p className="info">
+            <a className="author">Victor Henrique</a>
+            <time>2023-01-06 16:41</time>
+          </p>
+          <p className="summary">
+            The href attribute requires a valid value to be accessible. Provide
+            a valid, navigable address as the href value. If you cannot provide
+            a valid href, but still need the element to resemble a link, use a
+            button and change it with appropriate styles. Learn more:
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          ></img>
+        </div>
+        <div className="texts">
+          <h2>Tecnologia e Trabalho Home Office</h2>
+          <p className="info">
+            <a className="author">Victor Henrique</a>
+            <time>2023-01-06 16:41</time>
+          </p>
+          <p className="summary">
+            The href attribute requires a valid value to be accessible. Provide
+            a valid, navigable address as the href value. If you cannot provide
+            a valid href, but still need the element to resemble a link, use a
+            button and change it with appropriate styles. Learn more:
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
